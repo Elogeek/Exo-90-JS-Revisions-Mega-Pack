6a767a6efd38d857e2dd527c13322d3718b93e99
Elogeek <68891109+Elogeek@users.noreply.github.com>
2020-12-20 17:11:07 +0100

    exo 90

---
+++ exercice1.js
@@ -0,0 +1,12 @@
+let  nomVoiture = "Peugeot";
+let x = 50;
+
+let add = document.querySelector('#demo');
+let z = 5;
+let w = 10;
+let result = z + w;
+add.innerHTML = "z vaut : " + z + " y vaut: " + w + " le resultat est de = " +(z + w) ;
+let d = z + w;
+alert(z + w);
+
+let prenom = "John", nom = "Doe", age = 35;
